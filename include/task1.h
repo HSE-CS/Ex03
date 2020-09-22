da5540ed3f3da5b38557b91656325213fb394ec5
wityat <ttrttr449@gmail.com>
2020-09-22 15:13:43 +0300

    Shatiov Victor

---
+++ include/task1.h
@@ -0,0 +1,11 @@
+//
+// Created by victor on 22.09.2020.
+//
+
+#ifndef TASK1_TASK1_H
+#define TASK1_TASK1_H
+#include <cstdlib>
+
+
+int comp( const void* a, const void* b );
+#endif //TASK1_TASK1_H
---
+++ src/task1.cpp
@@ -0,0 +1,9 @@
+//
+// Created by victor on 22.09.2020.
+//
+
+int comp( const void* a, const void* b ) {
+    return ( *( int* )a - *( int* )b );
+}
+
+
