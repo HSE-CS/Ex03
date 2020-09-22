9085cc2a35246230a2816e1b4d6cb64dec725e0a
Sergey Boryaev <somsom.som2@yandex.ru>
2020-09-22 13:28:21 +0300

    it may work

--- src/main1.cpp
+++ src/main1.cpp
@@ -6,17 +6,20 @@ int main() {
     // Рандомизация по таймеру
     srand(time(nullptr));
     // генерируем случайные данные
-    generate( arr, arr + LARGE_SIZE, rnd() );
-    copy( arr, arr + LARGE_SIZE, arr_copy );
-    // засекаем время
-    time_t start = clock();
-    // выполняем сортировку, используя функцию qsort
-    qsort( arr, LARGE_SIZE, sizeof( int ), comp );
-    cout << "C quick-sort time elapsed: " << static_cast<double>( clock() - start ) / CLOCKS_PER_SEC << "\n";
-    // снова засекаем время
-    start = clock();
-    // выполняем сортировку, используя алгоритм С++
-    sort( arr_copy, arr_copy + LARGE_SIZE );
-    cout << "C++ quick-sort time elapsed: " << static_cast<double>( clock() - start ) / CLOCKS_PER_SEC << "\n";
+    for(int i=0; i<50; i++)
+    {
+        generate( arr, arr + (i+1)*2000, rnd() );
+        copy( arr, arr + (i+1)*2000, arr_copy );
+        // засекаем время
+        time_t start = clock();
+        // выполняем сортировку, используя функцию qsort
+        qsort( arr, (i+1)*2000, sizeof( int ), comp );
+        cout << "C quick-sort time for " << (i+1)*2000 << " elements elapsed: " << static_cast<double>( clock() - start ) / CLOCKS_PER_SEC << "\n";
+        // снова засекаем время
+        start = clock();
+        // выполняем сортировку, используя алгоритм С++
+        sort( arr_copy, arr_copy + (i+1)*2000);
+        cout << "C++ quick-sort time for " << (i+1)*2000 << " elements elapsed: " << static_cast<double>( clock() - start ) / CLOCKS_PER_SEC << "\n";
+    }
     return 0;
 }
