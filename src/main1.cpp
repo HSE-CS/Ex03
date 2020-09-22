202d08423886a345d0f207b79c08d716fac360bd
bekina-svetlana <bekina.swetlana@yandex.ru>
2020-09-22 16:10:08 +0300

    Bekina Svetlana

---
+++ src/main1.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "task1.h"
+
+int main() {
+	int arr[LARGE_SIZE];
+	int arr_copy[LARGE_SIZE];
+	// Рандомизация по таймеру
+	srand(time(nullptr));
+	// генерируем случайные данные
+	cout << "Размер\tC\tC++" << "\n";
+	for (int i = 0; i < 50; i++) {
+		int step = (rand() % ( LARGE_SIZE / 50  - 100) * (i + 1));
+		generate(arr, arr + LARGE_SIZE - step, rnd());
+		copy(arr, arr + LARGE_SIZE, arr_copy);
+		// засекаем время
+		time_t start = clock();
+		// выполняем сортировку, используя функцию qsort
+		qsort(arr, LARGE_SIZE - step, sizeof(int), comp);
+		cout << (int)(LARGE_SIZE - step) << "\t" << static_cast<double>(clock() - start) / CLOCKS_PER_SEC;
+		// снова засекаем время
+		start = clock();
+		// выполняем сортировку, используя алгоритм С++
+		sort(arr_copy, arr_copy + LARGE_SIZE - step);
+		cout << " \t" << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "\n";
+	}
+	return 0;
+}
