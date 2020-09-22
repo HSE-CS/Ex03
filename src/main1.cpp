786074d81fcf38068921a93120b60dafc6f959d3
skprpi <fatin.maksim@gmail.com>
2020-09-22 13:18:45 +0300

    Fatin Maxim

--- src/main1.cpp
+++ src/main1.cpp
@@ -9,12 +9,9 @@ using namespace std;
 int main() {
     int arr[LARGE_SIZE];
     int arr_copy[LARGE_SIZE];
-    // Рандомизация по таймеру
     srand(time(nullptr));
-    // генерируем случайные данные
     generate(arr, arr + LARGE_SIZE, rnd());
     copy(arr, arr + LARGE_SIZE, arr_copy);
-    // засекаем время
 
     cout << "quick-sort size / time" << endl;
     int size[1000] = {100};
@@ -23,24 +20,22 @@ int main() {
             size[i] = size[i - 1] + 100;
 
         time_t start = clock();
-        // выполняем сортировку, используя функцию qsort
         qsort(arr, size[i], sizeof(int), comp);
-        cout << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "\n";
+        //cout << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "\n";
     }
 
-    cout << "C++ quick-sort size/time elapsed" << endl;
+    //cout << "C++ quick-sort size/time elapsed" << endl;
 
     for (int i = 0; i < 1000; i++) {
         time_t start = clock();
-        // выполняем сортировку, используя алгоритм С++
         sort(arr_copy, arr_copy + size[i]);
-        cout << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "\n";
+        //cout << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "\n";
     }
 
-    cout << "SIZE" << endl;
+    /*cout << "SIZE" << endl;
     for (auto el : size)
         cout << el << endl;
-
+    */
     
     return 0;
 }
