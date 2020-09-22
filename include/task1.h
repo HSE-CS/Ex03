66bd698479770dbb2078398cfa625671a1ed420c
AlexShi23 <alieksandr_shirokov@list.ru>
2020-09-22 15:42:27 +0300

    Init

---
+++ include/task1.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+
+const size_t LARGE_SIZE = 100000;
+
+struct rnd {
+	int operator()() {
+		return rand() % LARGE_SIZE;
+	}
+};
+int comp(const void* a, const void* b);
---
+++ src/task1.cpp
@@ -0,0 +1,7 @@
+#include <math.h>
+#include <stdio.h>
+#include "task1.h"
+
+int comp(const void* a, const void* b) {
+	return (*(int*)a - *(int*)b);
+}
