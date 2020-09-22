35ebbdbedaf53d77fa2fedde2b986f7cc56ae466
Alexander Grachev <s89519011368@yandex.ru>
2020-09-22 12:44:59 +0300

    Text results

--- src/main1.cpp
+++ src/main1.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-const size_t LARGE_SIZE = 100000;
+const size_t LARGE_SIZE = 1000000;
 
 struct rnd {
     int operator()() {
@@ -24,22 +24,27 @@ int comp( const void* a, const void* b ) {
 }
 
 int main() {
+    ofstream file;
     int arr[LARGE_SIZE];
     int arr_copy[LARGE_SIZE];
     // Рандомизация по таймеру
     srand(time(nullptr));
-    // генерируем случайные данные
-    generate( arr, arr + LARGE_SIZE, rnd() );
-    copy( arr, arr + LARGE_SIZE, arr_copy );
-    // засекаем время
-    time_t start = clock();
-    // выполняем сортировку, используя функцию qsort
-    qsort( arr, LARGE_SIZE, sizeof( int ), comp );
-    cout << "C quick-sort time elapsed: " << static_cast<double>( clock() - start ) / CLOCKS_PER_SEC << "\n";
-    // снова засекаем время
-    start = clock();
-    // выполняем сортировку, используя алгоритм С++
-    sort( arr_copy, arr_copy + LARGE_SIZE );
-    cout << "C++ quick-sort time elapsed: " << static_cast<double>( clock() - start ) / CLOCKS_PER_SEC << "\n";
+    file.open("/Users/aleksandrgrachev/CLionProjects/Ex03/data/results.txt");
+    for (int i = 0; i < 50; i++){
+        // генерируем случайные данные
+        generate( arr, arr + (i * 1000), rnd() );
+        copy( arr, arr + (i * 1000), arr_copy );
+        // засекаем время
+        time_t start = clock();
+        // выполняем сортировку, используя функцию qsort
+        qsort( arr, (i * 1000), sizeof( int ), comp );
+        file << static_cast<double>( clock() - start ) / CLOCKS_PER_SEC << "\n";
+        // снова засекаем время
+        start = clock();
+        // выполняем сортировку, используя алгоритм С++
+        sort( arr_copy, arr_copy + (i * 1000) );
+        file << static_cast<double>( clock() - start ) / CLOCKS_PER_SEC << "\n";
+    }
+    file.close();
     return 0;
 }
