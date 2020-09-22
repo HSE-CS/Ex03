1c09f58bd48504f0bdee9435b15a233bcdfaf14c
Nikita Ziganshin <63304439+Liberator-1775@users.noreply.github.com>
2020-09-22 21:01:39 +0300

    Add files via upload

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,4 +1,30 @@
-//
-// Created by nikita on 22.09.2020.
-//
+#include "task1.h"
 
+using namespace std;
+
+int main() {
+    int arr[LARGE_SIZE];
+    int arr_copy[LARGE_SIZE];
+    // Рандомизация по таймеру
+    srand(time(nullptr));
+    ofstream fout;
+    fout.open("/home/nikita/CLionProjects/Ex03/data/data.txt");
+    for(int i = 0; i < 50; i++) {
+        // генерируем случайные данные
+        generate(arr, arr + i * 200, rnd());
+        copy(arr, arr + i * 200, arr_copy);
+        // засекаем время
+        time_t start = clock();
+        // выполняем сортировку, используя функцию qsort
+        qsort(arr, LARGE_SIZE, sizeof(int), comp);
+        fout << static_cast<double>( clock() - start ) / CLOCKS_PER_SEC << ";";
+        // снова засекаем время
+        start = clock();
+        // выполняем сортировку, используя алгоритм С++
+        sort(arr_copy, arr_copy + LARGE_SIZE);
+        fout << static_cast<double>( clock() - start ) / CLOCKS_PER_SEC << ";";
+        fout << i * 200 << "\n";
+    }
+    fout.close();
+    return 0;
+}
--- src/task1.cpp
+++ src/task1.cpp
@@ -2,3 +2,8 @@
 // Created by nikita on 22.09.2020.
 //
 
+#include "task1.h"
+
+int comp( const void* a, const void* b ) {
+    return ( *( int* )a - *( int* )b );
+}
