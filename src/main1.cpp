8807be10e46fe09df5dad56ecf2761cb4a294b23
natasha <igum.natasha@gmail.com>
2020-09-22 15:43:54 +0300

    fix bugs

--- src/main1.cpp
+++ src/main1.cpp
@@ -11,24 +11,24 @@
 int main() {
 	int arr[LARGE_SIZE];
 	int arr_copy[LARGE_SIZE];
-	// Рандомизация по таймеру
+	
 	srand(time(nullptr));
 	cout << "SIZE C C++\n";
 	for (int i = 0;i < 50;i++)
 	{
 
 		int step = 1900*i;
-		// генерируем случайные данные
+		
 		generate(arr, arr + LARGE_SIZE-step, rnd());
 		copy(arr, arr + LARGE_SIZE-step, arr_copy);
-		// засекаем время
+		
 		time_t start = clock();
-		// выполняем сортировку, используя функцию qsort
+		
 		qsort(arr, LARGE_SIZE-step, sizeof(int), comp);
 		cout << (int)(LARGE_SIZE - step) <<" "<< static_cast<double>(clock() - start) / CLOCKS_PER_SEC<< " ";
-		// снова засекаем время
+		
 		start = clock();
-		// выполняем сортировку, используя алгоритм С++
+		
 		sort(arr_copy, arr_copy + LARGE_SIZE-step);
 		cout << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "\n";
 	}
