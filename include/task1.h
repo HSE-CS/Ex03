15e4f4f72ad0db8a5bc2e9bae3d50b8cee7e671f
Ramazan <ramazankka220@gmail.com>
2020-09-23 02:07:30 +0300

    first try

---
+++ include/task1.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+const size_t LARGE_SIZE = 100000;
+
+struct rnd {
+    int operator()() {
+        return rand() % LARGE_SIZE;
+    }
+};
+
+int comp(const void* a, const void* b);
