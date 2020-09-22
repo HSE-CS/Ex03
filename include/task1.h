03fa6fa9e62fe50c6643857143f60ace9a784bc1
Pasha <paemshanov@edu.hse.ru>
2020-09-22 16:58:25 +0300

    Done! C was faster, both are almost linear

---
+++ include/task1.h
@@ -0,0 +1,14 @@
+#ifndef TASK3_H
+#define TASK3_H
+
+const size_t LARGE_SIZE =100000;
+
+struct rnd {
+    int operator()() {
+        return rand() % LARGE_SIZE;
+    }
+};
+
+int comp(const void* a, const void* b);
+
+#endif
