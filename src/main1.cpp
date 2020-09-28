8955f8e1e40604001a7fbcbfc71043e18488ed7a
DenisKabanov <dskabanov@edu.hse.ru>
2020-09-28 23:59:36 +0300

    b71

---
+++ src/main1.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "task1.h"
+
+using namespace std;
+
+int main() 
+{
+	cout.width(8);
+	cout << "size";
+	cout.width(12);
+	cout << "C qsort";
+	cout.width(12);
+	cout << "C++ sort" << endl;
+	check();
+}
