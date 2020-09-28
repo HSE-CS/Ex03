5fad785321669de3ccc6cb1fe1e84f0b6d7ef6d4
GlebNeshchetkin <glebneshchetkin@gmail.com>
2020-09-28 21:01:48 +0300

    Test5

--- include/task1.h
+++ include/task1.h
@@ -1,17 +1,19 @@
 #ifndef TASK1_H
 #define TASK1_H
+#include <cstdlib>
 using namespace std;
 
 const size_t LARGE_SIZE = 100000;
 
-#include <cstdlib>
+
 
 struct rnd {
     int operator()() {
+       // return rand() % LARGE_SIZE;
         return rand() % LARGE_SIZE;
     }
 };
 
 int comp(const void* a, const void* b);
-
+void result();
 #endif
--- src/task1.cpp
+++ src/task1.cpp
@@ -1,4 +1,4 @@
-#include "task1.h"
+﻿#include "task1.h"
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
@@ -11,4 +11,24 @@
 #include <iostream>
 int comp(const void* a, const void* b) {
     return (*(int*)a - *(int*)b);
+}
+void result()
+{
+    int arr[LARGE_SIZE];
+    int arr_copy[LARGE_SIZE];
+    // Рандомизация по таймеру
+    srand(time(nullptr));
+    // генерируем случайные данные
+    generate(arr, arr + LARGE_SIZE, rnd());
+    copy(arr, arr + LARGE_SIZE, arr_copy);
+    // засекаем время
+    time_t start = clock();
+    // выполняем сортировку, используя функцию qsort
+    qsort(arr, LARGE_SIZE, sizeof(int), comp);
+    cout << "C quick-sort time elapsed: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "\n";
+    // снова засекаем время
+    start = clock();
+    // выполняем сортировку, используя алгоритм С++
+    sort(arr_copy, arr_copy + LARGE_SIZE);
+    cout << "C++ quick-sort time elapsed: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "\n";
 }
