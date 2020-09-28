18dc16fcec26fe5c3f3cc87d9d9ad099c3dab52f
Polina <polpan01@mail.ru>
2020-09-29 01:55:14 +0300

    with results

--- include/task1.h
+++ include/task1.h
@@ -1,11 +1,5 @@
-#include <algorithm>
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
 #ifndef TASK1_H
 #define TASK1_H
-
-using namespace std;
 
 int comp( const void* a, const void* b );
 
--- src/task1.cpp
+++ src/task1.cpp
@@ -1,8 +1,5 @@
 #include "task1.h"
 
-using namespace std;
-
-
 int comp( const void* a, const void* b ) {
     return ( *( int* )a - *( int* )b );
 }
