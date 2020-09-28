89e8c9142d31e860f99b9cf7ab4d901b85288eb0
Niki <you@example.com>
2020-09-28 23:41:43 +0300

    is it f

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,29 +1,28 @@
+#include "task1.h"
 #include <algorithm>
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <iomanip>
-#include "task1.h"
 using namespace std;
-
 
 int main() {
     int arr[LARGE_SIZE];
     int arr_copy[LARGE_SIZE];
-    // Рандомизация по таймеру
-    srand(time(nullptr));
-    // генерируем случайные данные
-    generate(arr, arr + LARGE_SIZE, rnd());
-    copy(arr, arr + LARGE_SIZE, arr_copy);
-    // засекаем время
-    time_t start = clock();
-    // выполняем сортировку, используя функцию qsort
-    qsort(arr, LARGE_SIZE, sizeof(int), comp);
-    cout << "C quick-sort time elapsed: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "\n";
-    // снова засекаем время
-    start = clock();
-    // выполняем сортировку, используя алгоритм С++
-    sort(arr_copy, arr_copy + LARGE_SIZE);
-    cout << "C++ quick-sort time elapsed: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "\n";
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        srand(time(nullptr));
+        generate(arr, arr + STEP * i, rnd());
+        copy(arr, arr + STEP * i, arr_copy);
+        time_t start = clock();
+        qsort(arr, STEP * i, sizeof(int), comp);
+        cout << STEP * i << " " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << " "
+            << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "\n";
+        start = clock();
+        sort(arr_copy, arr_copy + STEP * i);
+    }
+
     return 0;
 }
