131a06f8d2487315bcb8023eb49e89dce8a8d777
Irina Zarubina <63425301+IrinaZarubina@users.noreply.github.com>
2020-09-29 00:03:37 +0300

    Irina Zarubina

---
+++ include/task1.h
@@ -0,0 +1,16 @@
+#ifndef TASK1_H
+#define TASK1_H
+using namespace std;
+
+
+const size_t LARGE_SIZE = 120000;
+
+struct rnd {
+	int operator()() {
+		return rand() % LARGE_SIZE;
+	}
+};
+
+int comp(const void* a, const void* b);
+
+#endif
