56326bff6417f881b89b819d98515e56b95b35be
k-dash6 <71211839+k-dash6@users.noreply.github.com>
2020-09-29 00:13:36 +0300

    b75

--- include/task1.h
+++ include/task1.h
@@ -5,4 +5,20 @@
 #ifndef TASK1_TASK1_H
 #define TASK1_TASK1_H
 
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+const unsigned int LARGE_SIZE = 500000;
+
+struct rnd {
+    unsigned int operator()() {
+        return rand() % LARGE_SIZE;
+    }
+};
+
+int comp(const void* a, const void* b);
 #endif //TASK1_TASK1_H
--- src/task1.cpp
+++ src/task1.cpp
@@ -1,4 +1,7 @@
 //
 // Created by kdash on 28.09.2020.
 //
-
+#include "task1.h"
+int comp(const void* a, const void* b){
+    return (*(int*)a - *(int*)b);
+}
