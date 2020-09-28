5f8b821adb38eaecd69e25e58cb6d16ec4ec8e46
Karina Mashanova <karmen.01@inbox.ru>
2020-09-29 00:19:09 +0300

    Test1

---
+++ include/task1.h
@@ -0,0 +1,17 @@
+#ifndef TASK1_H
+#define TASK1_H
+
+const size_t LARGE_SIZE = 100000;
+
+struct rnd {
+	int operator()() {
+		return rand() % 100000;
+	}
+};
+
+int comp(const void* a, const void* b);
+
+
+
+
+#endif TASK1_H
