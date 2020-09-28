a75fb3fe11a4ca08887deba376b9ef9c17b8091d
Diana <drsharibzhanova@edu.hse.ru>
2020-09-28 22:55:26 +0300

    Хех

---
+++ src/task1.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "task1.h"
+
+
+int comp( const void* a, const void* b ) {
+    return ( *( int* )a - *( int* )b );
+}
