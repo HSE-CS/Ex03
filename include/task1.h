e42996d3af7f609b3ffa31cabfe696f456221b0a
Urtyukov Ilya <urtilya@gmai.com>
2020-09-29 19:42:20 +0400

    Уртюков Илья 19ПМИ2

---
+++ include/task1.h
@@ -0,0 +1,4 @@
+#ifndef Task1_H
+#define Task1_H
+int comp( const void* a, const void* b );
+#endif
---
+++ src/task1.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "task1.h"
+
+using namespace std;
+
+const size_t LARGE_SIZE = 100000;
+
+struct rnd {
+    int operator()() {
+        return rand() % LARGE_SIZE;
+    }
+};
+
+int comp( const void* a, const void* b ) {
+    return ( *( int* )a - *( int* )b );
+}
