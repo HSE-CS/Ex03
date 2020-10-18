a07f557394e706bdbc646dad294c4e4d83b3c34e
DMA <dima.olenenev.01@mail.ru>
2020-10-18 23:33:45 +0300

    Оленев Дмитрий

---
+++ src/main1.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "task1.h"
+
+using namespace std;
+
+struct rnd {
+    int operator()() {
+        return rand() % LARGE_SIZE;
+    }
+};
+
+
+int main() {
+    int arr[LARGE_SIZE];
+    int arr_copy[LARGE_SIZE];
+    srand(time(nullptr));
+    generate( arr, arr + LARGE_SIZE, rnd() );
+    copy( arr, arr + LARGE_SIZE, arr_copy );
+    time_t start = clock();
+    qsort( arr, LARGE_SIZE, sizeof( int ), comp );
+    cout << "C quick-sort time elapsed: " << static_cast<double>( clock() - start ) / CLOCKS_PER_SEC << "\n";
+    start = clock();
+    sort( arr_copy, arr_copy + LARGE_SIZE );
+    cout << "C++ quick-sort time elapsed: " << static_cast<double>( clock() - start ) / CLOCKS_PER_SEC << "\n";
+    return 0;
+} 
